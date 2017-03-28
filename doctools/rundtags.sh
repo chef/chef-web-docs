bb455e07299a8c8a73fe960ea7d4dea4c5661bfe
Stephan Renatus <srenatus@chef.io>
2017-03-28 12:39:46 +0200

    copyedit rundtags.sh
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- doctools/rundtags.sh
+++ doctools/rundtags.sh
@@ -9,7 +9,7 @@ DTAGSRESULT=$?
 if [ $DTAGSRESULT -ne 0 ]; then
   (>&2 echo 'dtags checked failed')
   (>&2 echo '  The Chef docs team will make tagged regions consistent prior' )
-  (>&2 echo '  prior to merging your PR (may take a few days longer), or you can' )
+  (>&2 echo '  to merging your PR (may take a few days longer), or you can' )
   (>&2 echo '  use the dtags tool yourself. See chef-web-docs README.md for more ' )
   (>&2 echo '  information about tagged regions. ' )
 fi
