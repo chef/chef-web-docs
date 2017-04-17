657e5660b1cd33e0f0a0b074ebc37e4c1923f0ae
David Wrede <dwrede@chef.io>
2017-04-17 13:25:53 -0700

    Python cookbook has been deprecated, so using poise-python instead
    Signed-off-by: David Wrede <dwrede@chef.io>

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe 'delivery-truck::default'
 include_recipe 'cia_infra::aws_prereq'
-include_recipe 'python::default'
+include_recipe 'poise-python::default'
 
 execute 'pip install -r requirements.txt' do
   cwd node['delivery']['workspace']['repo']
