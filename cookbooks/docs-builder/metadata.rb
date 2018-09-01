dd0beae344b2027775882ba7fe3e9cceb5ba1ce0
kagarmoe <kgarmoe@chef.io>
2018-08-31 17:46:55 -0700

    Removes /cookbooks/docs-builder
    Signed-off-by: kagarmoe <kgarmoe@chef.io>

--- cookbooks/docs-builder/metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'docs-builder'
-maintainer 'Chef Docs Team'
-maintainer_email 'docs@chef.io'
-license 'Apache-2.0'
-description 'Installs/Configures docs-builder'
-long_description 'Installs/Configures docs-builder'
-version '0.3.3'
