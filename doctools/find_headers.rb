cb28119cb6c1bf19d5e3bd4bd06f9111f7324a52
kagarmoe <kgarmoe@chef.io>
2019-07-15 13:13:05 -0700

    cleans up code
    Signed-off-by: kagarmoe <kgarmoe@chef.io>

--- doctools/find_headers.rb
+++ doctools/find_headers.rb
@@ -5,26 +5,11 @@ require 'nokogiri'
 require 'open-uri'
 require 'uri'
 
-## Create HTML from .rst files
-
 docs =  Dir.entries("chef_master/source/")
 resources = docs.select {|s| s.include? 'resource_'}
-
-resources.each do |f|
-  f.chomp!("rst")
-end
-
-html = resources.each do |f|
-  f << "html"
-end
-
 BASE_URL="https://docs.chef.io/"
-html.to_s
-url = html.each do |f|
-  f.prepend("https://docs.chef.io/")
-end
 
-## Make sure file is empty
+## Make sure CSV file exists and is empty
 if File.exist?("doctools/headers.csv")
   File.delete("doctools/headers.csv")
   puts "Deleted old file"
@@ -33,8 +18,15 @@ else
   "Created new file"
 end
 
-# Scrapes headers from HTML and save in CSV
-url.each do |docspage|
+## Creates URLs from .rst files
+resources.each do |f|
+  f.chomp!("rst")
+  f << "html"
+  f.prepend("#{ BASE_URL }")
+end
+
+# Scrapes headers from HTML and saves in CSV
+resources.each do |docspage|
   docspage.to_s
   page = Nokogiri::HTML.parse(open("#{ docspage }"))
   page.css('div.body').collect do |headers|
