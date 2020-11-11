e84a94915cc21e5fa1a8686056421cad03e806b8
kagarmoe <kgarmoe@chef.io>
2020-11-11 10:22:53 -0800

    Add Google Analytics tag
    Signed-off-by: kagarmoe <kgarmoe@chef.io>

---
+++ themes/docs-new/assets/js/gtag.js
@@ -0,0 +1,9 @@
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2L57JLVKNZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2L57JLVKNZ');
+</script>
