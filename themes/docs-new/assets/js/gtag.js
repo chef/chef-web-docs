d697e296917ab161e1536f4a5720131465a63eac
kagarmoe <kgarmoe@chef.io>
2020-11-12 13:17:24 -0800

    Remove unused file
    Signed-off-by: kagarmoe <kgarmoe@chef.io>

--- themes/docs-new/assets/js/gtag.js
+++
@@ -1,9 +0,0 @@
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-2L57JLVKNZ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-2L57JLVKNZ');
-</script>
