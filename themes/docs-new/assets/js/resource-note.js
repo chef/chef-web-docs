429b3b52d0393f47fa40fe3275ee4d6eeb0e464b
Ian Maddaus <IanMadd@users.noreply.github.com>
2021-04-07 14:33:30 -0700

    Clean up javascript and node (#3043)
    Signed-off-by: IanMadd <Ian.Maddaus@progress.com>

--- themes/docs-new/assets/js/resource-note.js
+++ themes/docs-new/assets/js/resource-note.js
@@ -1,8 +1,7 @@
-function resource_note_function(id) {
-  var x = document.getElementById(id);
-  if (x.className.indexOf("note-show") == -1) {
-    x.className += " note-show";
-  } else { 
-    x.className = x.className.replace(" note-show", "");
-  }
+// Show and hide note at top of each resource page telling
+// users where they can edit resource page YAML files
+
+function resource_note_function(elementID) {
+  var e = document.getElementById(elementID);
+  e.style.display = e.style.display === 'block' ? 'none' : 'block';
 }
