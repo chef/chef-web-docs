e5c420a84315e8967b9a9e9a872f36b27350e80f
IanMadd <Ian.Maddaus@progress.com>
2021-04-09 15:26:38 -0700

    Add faceted search with Swiftype
    Signed-off-by: IanMadd <Ian.Maddaus@progress.com>

--- go.mod
+++ go.mod
@@ -11,9 +11,13 @@ require (
 	github.com/chef/cookstyle/docs-chef-io v0.0.0-20210324200148-f941dae22320 // indirect
 	github.com/chef/desktop-config/docs-chef-io v0.0.0-20210219180412-c50c4ad0869d // indirect
 	github.com/chef/effortless/docs-chef-io v0.0.0-20210212122821-321ee6f6699a // indirect
+	github.com/cowboy/jquery-hashchange v0.0.0-20100902193700-0310f3847f90 // indirect
 	github.com/habitat-sh/habitat/components/docs-chef-io v0.0.0-20210402191111-4b56ffe5d933 // indirect
 	github.com/inspec/inspec v4.29.3+incompatible // indirect
 	github.com/inspec/inspec/docs-chef-io v0.0.0-20210408194811-b3a0687fbe3e // indirect
+	github.com/swiftype/swiftype-autocomplete-jquery v0.0.0-20190222215504-a90008d64b30 // indirect
+	github.com/swiftype/swiftype-search-jquery v1.1.0 // indirect
+	github.com/twitter/hogan.js v3.0.2+incompatible // indirect
 )
 
 //replace github.com/chef/effortless/docs-chef-io => ../effortless/docs-chef-io
---
+++ themes/docs-new/assets/js/swiftype-config.js
@@ -0,0 +1,208 @@
+/*
+  Sources of information:
+  https://github.com/swiftype/swiftype-search-jquery
+  https://github.com/swiftype/swiftype-autocomplete-jquery
+  https://github.com/swiftype/swiftype-faceted-search-example
+  https://swiftype.com/documentation/site-search/guides/jquery
+  https://swiftype.com/documentation/site-search/guides/faceted-search
+*/
+
+$(document).ready(function() {
+
+  // Swiftype UI elements //
+
+  var showSearchModal = function() {
+    $("#swiftype-modal").show(500);
+  }
+
+  var hideSearchModal = function() {
+    $("#swiftype-modal").hide(250);
+    $("#swiftype-custom-facets input:checkbox[class='product-filter']").prop( "checked", false );
+    window.location.hash = "";
+  }
+
+  $("#swiftype-close-button").click(function(){
+    hideSearchModal();
+  });
+
+  $("#swiftype-custom-facets-toggle-button").click(function(){
+    if ($(window).width() < 768) {
+      $("#swiftype-custom-facets").toggle(500);
+      $("#swiftype-custom-facets-toggle-chevron").find('svg').toggle();
+    }
+  });
+
+  $('#swiftype-facet-infra').on('change',function(){
+    $('#swiftype-facet-client, #swiftype-facet-server').prop('checked',$(this).prop('checked'));
+  });
+
+  $(window).resize(function() {
+    if ($(this).width() < 768) {
+      $('#swiftype-custom-facets').hide();
+    } else {
+      $('#swiftype-custom-facets').show();
+      }
+  });
+
+  const searchHashRegex = /^#stq./;
+  const searchStringMatchRegex = /^#stq=(.*)/;
+  const searchPageNumRegex = /&stp=\d{1,3}$/;
+
+
+  var processLocationHash = function() {
+    var hash = searchStringMatchRegex.exec(window.location.hash)[1];
+    pageNumTrue = searchPageNumRegex.exec(hash);
+    if (pageNumTrue) {
+      var matchString = decodeURIComponent(hash.substring(0,pageNumTrue.index));
+    } else {
+      var matchString = decodeURIComponent(hash);
+    };
+    $('input#swiftype-search-form-modal').val(matchString);
+  };
+  // Watch for hashchange
+  // #stq=[whatever]&stp=[number]
+  // #stq=release notes&stp=1
+  // it matches #stq
+
+
+  $(window).on('hashchange', function() {
+    if (searchHashRegex.test(window.location.hash)) {
+      processLocationHash();
+      showSearchModal();
+    }
+  });
+
+  if (searchHashRegex.test(window.location.hash)) {
+    processLocationHash();
+    showSearchModal();
+  };
+
+  // Autocomplete UI elements
+  $("input.swiftype-search-input").on('keypress', function (event) {
+   if (event.keycode == 13 || event.which == 13) {
+      event.preventDefault();
+      window.location.hash = 'stq=' + encodeURIComponent($(this).val()) + '&stp=1';
+    };
+  });
+
+  /////////////////////////
+  // Handle Search Results
+  /////////////////////////
+  var searchConfig = {
+    "facets": {}
+  };
+
+  /////////
+  // Handle Regular Search Results
+  /////////
+  var resultTemplate = Hogan.compile([
+    "<a class='swiftype-result' href='{{url}}'>",
+      "<span class='st-result-title'>{{title}}</span>",
+      // "<span class='st-result-detail'>Product: {{product}} {{{sections}}}<span class='st-result-detail-body'> • {{{body}}}</span></span>",
+      "<span class='st-result-detail'>{{{sections}}}<span class='st-result-detail-body'> • {{{body}}}</span></span>",
+    "</a>"
+  ].join('') );
+
+  var customRenderFunction = function(document_type, item) {
+    var
+      data = {
+        title: item['title'],
+        product: item['chef-product'],
+        url: item['url'],
+        sections: item['highlight']['sections'],
+        body: item.highlight['body'],
+      };
+    return resultTemplate.render(data);
+  };
+
+  // Update the hash value in window.location //
+  var reloadResults = function() {
+    window.location.hash = window.location.hash.replace(/stp=[^&]*/i, 'stp=1'); // Reset to page 1
+    $(window).hashchange();
+  };
+
+  //Checking the Infra checkbox will also check the Infra Client and Infra Server checkboxes.
+  $('#swiftype-facet-infra').on('change',function(){
+    $('#swiftype-facet-client, #swiftype-facet-server').prop('checked',$(this).prop('checked'));
+  });
+
+  // When the product facet checkboxes are checked/unchecked, update the array of products and reload the results
+  $("#swiftype-custom-facets input:checkbox[class='product-filter']").change(function() {
+    searchConfig.facets['chef-products'] = $("#swiftype-custom-facets input:checkbox[name='chef-product']:checked").map(function() {
+      return $(this).val()
+    }).get();
+    reloadResults();
+  });
+
+  // Returns an array of the chef-products to filter by
+  var readFilters = function () {
+    return {
+      "page": {
+        "chef-product": searchConfig.facets['chef-products'],
+      }
+    }
+  };
+
+  var highlightFields = {
+    "page": {
+      "sections":{"size": 200, "fallback":true},
+      "body":{"size": 250, "fallback": true},
+    }
+  }
+
+  /////////
+  // Handle Autocomplete Search Results
+  /////////
+
+  var resultAutocompleteTemplate = Hogan.compile([
+    "<a class='swiftype-auto-result' href='{{url}}'>",
+      "<span class='swiftype-auto-result-title'>{{title}}</span>",
+      "<span class='swiftype-auto-result-detail'>{{{sections}}}</span>",
+    "</a>"
+  ].join('') );
+
+  var customAutocompleteFunction = function(document_type, item) {
+    var
+      data = {
+        title: item['title'],
+        url: item['url'],
+        sections: item['highlight']['sections'],
+      };
+    return resultAutocompleteTemplate.render(data);
+  };
+
+  var autoHighlightFields = {
+    "page": {
+      "sections":{"size": 200, "fallback":true},
+      "body":{"size": 250, "fallback": true},
+    }
+  }
+
+  /*
+    The swiftypeSearch function is from the Swiftype jQuery Plugin - "jquery.swiftype.search.js"
+    https://swiftype.com/documentation/site-search/guides/jquery
+    https://github.com/swiftype/swiftype-search-jquery
+  */
+
+  $('#swiftype-search-form-modal').swiftypeSearch({
+    resultContainingElement: '#swiftype-search-results',
+    engineKey: 'mXqaEKYsv--xS9fcY3Jz',
+    renderFunction: customRenderFunction,
+    facets: { page: ['chef-product'] },
+    filters: readFilters,
+    perPage: 8,
+    highlightFields: highlightFields,
+  });
+
+  /*
+    The swiftype function is from the autocomplete plugin - "jquery.swiftype.autocomplete.js"
+    https://swiftype.com/documentation/site-search/guides/jquery
+    https://github.com/swiftype/swiftype-autocomplete-jquery
+  */
+
+  $('.swiftype-search-autocomplete').swiftype({
+    engineKey: 'mXqaEKYsv--xS9fcY3Jz',
+    highlightFields: autoHighlightFields,
+    renderFunction: customAutocompleteFunction,
+  });
+});
