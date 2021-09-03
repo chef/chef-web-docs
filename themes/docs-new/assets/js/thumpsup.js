38cef4cb800e425ce85fa8f6d3071b2343816203
Dishank Tiwari <dtiwari@progress.com>
2021-09-03 19:08:21 +0530

    css check for the thumbsup and down
    Signed-off-by: Dishank Tiwari <dtiwari@progress.com>

--- themes/docs-new/assets/js/thumpsup.js
+++ themes/docs-new/assets/js/thumpsup.js
@@ -1,45 +1,5 @@
-const yesButton = document.querySelector('.feedback-yes');
-const noButton = document.querySelector('.feedback-no');
-
-const disableButtons = () => {
-  yesButton.disabled = true;
-  noButton.disabled = true;
-};
-
-function thanksFeedback(button){
-  button.blur();
-  buttonText = button.innerText
-  button.innerText = 'Thanks for the feedback!'
-  setTimeout(function () {
-    button.innerText = buttonText;
-  }, 2000);
-}
-
-const sendFeedback = (value) => {
-  if (typeof ga !== 'function') return;
-  const args = {
-    command: 'send',
-    hitType: 'event',
-    category: 'Feedback Buttons',
-    action: 'click',
-    label: window.location.pathname,
-    value: value
-  };
-  ga(args.command, args.hitType, args.category, args.action, args.label, args.value);
-};
-
-if (yesButton !== null ) {
-  yesButton.addEventListener('click', () => {
-    thanksFeedback(yesButton);
-    disableButtons();
-    sendFeedback(1);
-  });
-}
-
-if (noButton !== null ) {
-  noButton.addEventListener('click', () => {
-    thanksFeedback(noButton);
-    disableButtons();
-    sendFeedback(0);
-  });
-}
+$('.like, .dislike').on('click', function() {
+  event.preventDefault();
+  $('.active').removeClass('active');
+  $(this).addClass('active');
+});
