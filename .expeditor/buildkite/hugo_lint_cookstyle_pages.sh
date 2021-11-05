3ac9aa9400279ad715ec5571857ae18f01cbd1af
Ian Maddaus <ian.maddaus@progress.com>
2021-11-05 14:59:47 -0400

    Add lint to check for Cookstyle pages
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>

---
+++ .expeditor/buildkite/hugo_lint_cookstyle_pages.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Verify that each Cookstyle YAML file in _vendor/github.com/chef/cookstyle/docs-chef-io/data/cookstyle
+# has a matching Markdown file in content/workstation/cookstyle.
+
+# You can regenerate all the Cookstyle Markdown files by running `make cookstyle_cops_pages`.
+# Individual Cookstyle Markdown pages can be created by running `hugo new -k cookstyle_cop content/workstation/cookstyle/NAME_OF_COOKSTYLE_YAML_FILE.md`.
+
+set -euo pipefail
+
+dataDir=_vendor/github.com/chef/cookstyle/docs-chef-io/data/cookstyle
+markdownDir=./content/workstation/cookstyle
+LINT_STATUS=0
+for f in $(ls ${dataDir})
+    do mdFile="${f/.yml/.md}"
+    mdFile="${mdFile/cops_/}"
+    mdFilePath="${markdownDir}/${mdFile}"
+    if test ! -f "${mdFilePath}"; then
+        echo "${mdFilePath} is missing."
+        LINT_STATUS=1
+    fi
+done
+
+if test ! -f "content/workstation/cookstyle/cops.md"; then
+    echo "content/workstation/cookstyle/cops.md is missing."
+    LINT_STATUS=1
+fi
+
+if [ "$LINT_STATUS" == 1 ]; then
+    echo "At least one Cookstyle Markdown file is missing from content/workstation/cookstyle."
+    echo "Regenerate all Cookstyle Markdown files by running 'make cookstyle_cops_pages', or make individual Cookstyle Markdown pages"
+    echo "by running 'hugo new -k cookstyle_cop content/workstation/cookstyle/NAME_OF_COOKSTYLE_YAML_FILE.md'."
+    exit 1
+else
+    echo "Success! All Cookstyle Markdown files exist."
+    exit 0
+fi
