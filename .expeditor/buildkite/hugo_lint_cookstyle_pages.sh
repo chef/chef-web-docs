64db474a9013a753f91cec8df1ff0b417a63a44e
Ian Maddaus <IanMadd@users.noreply.github.com>
2021-11-10 10:18:12 -0500

    Add lint to check for Cookstyle pages (#3540)
    * Add lint to check for Cookstyle pages
    
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>
    
    * Change to glob path
    
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>
    
    * Testing things
    
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>
    
    * Rename cookstyle pages
    
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>
    
    * Rename files again
    
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>
    
    * clean up lint script
    
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>
    
    Co-authored-by: Kimberly Garmoe <kgarmoe@chef.io>

---
+++ .expeditor/buildkite/hugo_lint_cookstyle_pages.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Verify that each Cookstyle YAML file in _vendor/github.com/chef/cookstyle/docs-chef-io/data/cookstyle
+# has a matching Markdown file in content/workstation/cookstyle.
+
+# You can regenerate all the Cookstyle Markdown files by running `make cookstyle_cops_pages`.
+# Individual Cookstyle Markdown pages can be created by running `hugo new -k cookstyle_cop content/workstation/cookstyle/NAME_OF_COOKSTYLE_YAML_FILE_WITHOUT_YAML_SUFFIX_AND_COPS_PREFIX.md`.
+# For example, to create a Markdown page for the cops_chef_correctness_blockguardwithonlystring.yml file, run `hugo new -k cookstyle_cop content/workstation/cookstyle/chef_correctness_blockguardwithonlystring.md`.
+
+
+set -euo pipefail
+
+dataDir=_vendor/github.com/chef/cookstyle/docs-chef-io/data/cookstyle
+markdownDir=./content/workstation/cookstyle
+LINT_STATUS=0
+for file in _vendor/github.com/chef/cookstyle/docs-chef-io/data/cookstyle/*
+    do mdFile="${file/.yml/.md}"
+    mdFile="${mdFile/cops_/}"
+    mdFile="${mdFile/_vendor\/github.com\/chef\/cookstyle\/docs-chef-io\/data\/cookstyle/content/workstation/cookstyle}"
+    if test ! -f "${mdFile}"; then
+        echo "${mdFile} is missing."
+        LINT_STATUS=1
+    fi
+done
+
+if test ! -f "content/workstation/cookstyle/cops.md"; then
+    echo "content/workstation/cookstyle/cops.md is missing."
+    LINT_STATUS=1
+fi
+
+if [ "$LINT_STATUS" == 1 ]; then
+    echo "At least one Cookstyle Markdown file is missing from content/workstation/cookstyle."
+    echo "Regenerate all Cookstyle Markdown files by running 'make cookstyle_cops_pages', or make individual Cookstyle Markdown pages"
+    echo "by running 'hugo new -k cookstyle_cop content/workstation/cookstyle/NAME_OF_COOKSTYLE_YAML_FILE_WITHOUT_YAML_SUFFIX_AND_COPS_PREFIX.md'."
+    echo "For example, to create a Markdown page for the cops_chef_correctness_blockguardwithonlystring.yml file,"
+    echo "run 'hugo new -k cookstyle_cop content/workstation/cookstyle/chef_correctness_blockguardwithonlystring.md'."
+    exit 1
+else
+    echo "Success! All Cookstyle Markdown files exist."
+    exit 0
+fi
