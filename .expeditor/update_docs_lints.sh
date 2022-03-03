00d3812d249334ae235446e3badf95b33e0b785e
Kimberly Garmoe <kimberly.garmoe@progress.com>
2022-03-03 11:31:26 -0800

    Single source the docs lints (#3732)
    * Single source the docs lints
    
    Signed-off-by: kagarmoe <kimberly.garmoe@progress.com>
    
    * loop
    
    Signed-off-by: kagarmoe <kimberly.garmoe@progress.com>
    
    * bash fix
    
    Signed-off-by: kagarmoe <kimberly.garmoe@progress.com>
    
    * config fix
    
    Signed-off-by: kagarmoe <kimberly.garmoe@progress.com>
    
    * test
    
    Signed-off-by: kagarmoe <kimberly.garmoe@progress.com>

---
+++ .expeditor/update_docs_lints.sh
@@ -0,0 +1,27 @@
+set -eou pipefail
+
+# test on one repo
+repos=[chef/workstation]
+# repos=["chef/automate", "chef/chef-server", "chef/desktop-config", "chef/supermarket", "chef/workstation", "inspec/inspec", "inspec/CLOUD_RESOURCES", "habitat-sh/habitat"]
+
+for i in ${repos}
+
+do
+  repo=$i
+  branch="expeditor/update_docs_lints_${EXPEDITOR_REPO}_${EXPEDITOR_DATE}"
+  git checkout -b "$branch"
+  git add .
+
+  # give a friendly message for the commit and make sure it's noted for any future
+  # audit of our codebase that no DCO sign-off is needed for this sort of PR since
+  # it contains no intellectual property
+
+  dco_safe_git_commit "Update ${repo} docs lints."
+
+  open_pull_request
+
+  # Get back to main and cleanup the leftovers - any changed files left over at
+  # the end of this script will get committed to main.
+  git checkout -
+  git branch -D "$branch"
+done
