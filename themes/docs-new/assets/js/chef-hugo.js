c304886b0ac134dcd29a8b3e733a22dd7e8f9fd8
Ian Maddaus <IanMadd@users.noreply.github.com>
2023-03-27 14:46:16 -0400

    Fix heading IDs in release notes (#4076)
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>

--- themes/docs-new/assets/js/chef-hugo.js
+++ themes/docs-new/assets/js/chef-hugo.js
@@ -1,3 +1,13 @@
 // Initialize Foundation
 // https://get.foundation/sites/docs/installation.html
-$(document).foundation();
+// Also add links to heading IDs
+
+(function (win, doc,$) {
+
+  $(document).foundation();
+
+  $(".prose > :header" ).add(".chef-product-version > :header" ) .each(function () {
+    $(this).append("<a href=\"\#" + $(this).attr("id") + "\"><i class=\"fas fa-link\"></i></a>");
+  });
+
+}(window, document, jQuery));
