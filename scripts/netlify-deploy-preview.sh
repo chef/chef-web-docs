9921fe9e3198887c5d03c9dde212d908d30b95a8
Ian Maddaus <ian.maddaus@progress.com>
2024-04-16 12:47:45 -0400

    Fix path problems with other repos
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>

--- scripts/netlify-deploy-preview.sh
+++ scripts/netlify-deploy-preview.sh
@@ -20,7 +20,7 @@ echo Branch deploy URL: $DEPLOY_PRIME_URL
 curl -LJO https://github.com/sass/dart-sass/releases/download/$DART_SASS_VERSION/dart-sass-$DART_SASS_VERSION-linux-x64.tar.gz
 tar -xf dart-sass-$DART_SASS_VERSION-linux-x64.tar.gz
 rm dart-sass-$DART_SASS_VERSION-linux-x64.tar.gz
-export PATH=/opt/build/repo/dart-sass:$PATH
+export PATH=$(pwd)/dart-sass:$PATH
 
 npm install
 hugo --gc --minify --enableGitInfo --buildFuture
