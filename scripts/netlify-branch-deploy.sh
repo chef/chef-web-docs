b6530bd837840605ef9ac19bce331928af07eca5
Ian Maddaus <ian.maddaus@progress.com>
2025-02-17 16:16:03 -0500

    Get NPM deps in Netlify build
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>

--- scripts/netlify-branch-deploy.sh
+++ scripts/netlify-branch-deploy.sh
@@ -20,5 +20,7 @@ tar -xf dart-sass-$DART_SASS_VERSION-linux-x64.tar.gz
 rm dart-sass-$DART_SASS_VERSION-linux-x64.tar.gz
 export PATH=/opt/build/repo/dart-sass:$PATH
 
+npm ls
+
 npm install
 hugo --gc --minify --enableGitInfo
